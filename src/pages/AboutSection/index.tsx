7c9cfca1ddc01e9be2e4bb5eead5331c34809022
Pedro Henrique <pedrohenriquefrancadedeus@gmail.com>
2023-08-05 23:21:41 -0300

    refactor: change text size

--- src/pages/AboutSection/index.tsx
+++ src/pages/AboutSection/index.tsx
@@ -4,14 +4,14 @@ export const AboutSection = () => {
   return (
     <div className='px-12 pt-20 transition-colors bg-zinc-50 dark:bg-neutral-800'>
       <section>
-        <h1 className='w-24 text-5xl transition-colors font-familjen text-zinc-800 dark:text-zinc-50'>
+        <h1 className='w-24 text-4xl transition-colors font-familjen text-zinc-800 dark:text-zinc-50'>
           A bit about me
         </h1>
 
-        <HandwriteArrow className='h-12 transition-colors w-52 text-zinc-800 dark:text-zinc-50' />
+        <HandwriteArrow className='h-12 transition-colors w-44 text-zinc-800 dark:text-zinc-50' />
       </section>
 
-      <p className='mt-10 text-xl text-justify transition-colors font-inter text-zinc-700 dark:text-zinc-100'>
+      <p className='mt-10 text-xl transition-colors font-inter text-zinc-700 dark:text-zinc-100'>
         Hey! I'm a 22-year-old frontend developer with 3 years of experience. I
         specialize in React.js and React Native, crafting smooth animations with
         Framer Motion, Moti, and Keyframes. I'm all about responsive design and
