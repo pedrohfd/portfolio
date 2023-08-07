9a25164bda6536d91be082f1c3093f0566d064d5
Pedro Henrique <pedrohenriquefrancadedeus@gmail.com>
2023-08-07 19:24:27 -0300

    feat: add copyright to footer

--- src/pages/FooterSection/index.tsx
+++ src/pages/FooterSection/index.tsx
@@ -1,12 +1,16 @@
 export const FooterSection = () => {
   return (
-    <footer className='pt-8 pb-10 border-t border-zinc-200'>
+    <footer className='pt-8 pb-24 border-t border-zinc-200'>
       <p className='text-4xl font-bold text-center transition-colors font-inter text-neutral-800 dark:text-zinc-50'>
         ph <span className='-ml-3 text-purple-500'>.</span>
       </p>
 
       <p className='mt-4 text-lg text-center transition-colors font-inter text-neutral-800 dark:text-zinc-50'>
-        Created by: Pedro Henrique
+        Created by Pedro Henrique
+      </p>
+
+      <p className='text-lg text-center transition-colors font-inter text-neutral-800 dark:text-zinc-50'>
+        Copyright © 2023 Me. All rights reserved
       </p>
     </footer>
   )
